0b2b24c4f79a0ac0f3366256042c229cd3764907
Claire Thorington <claire.thorington@gmail.com>
2022-07-26 17:19:27 -0700

    finish lesson 34

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
@@ -21,6 +22,13 @@ namespace ToDoList.Controllers
       return View(model);
     }
 
+    public ActionResult Create()
+    {
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
+      return View();
+    }
+
+    [HttpPost]
     public ActionResult Create(Item item)
     {
       _db.Items.Add(item);
@@ -37,6 +45,7 @@ namespace ToDoList.Controllers
     public ActionResult Edit(int id)
     {
       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      ViewBag.CategoryId = new SelectList(_db.Categories, "CategoryId", "Name");
       return View(thisItem);
     }
 
@@ -53,6 +62,7 @@ namespace ToDoList.Controllers
       var thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
       return View(thisItem);
     }
+
     [HttpPost, ActionName("Delete")]
     public ActionResult DeleteConfirmed(int id)
     {
